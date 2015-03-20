ab12508789887fce5dc91e25daf9b131d5212616
csy <csy@chenhuan.local>
2015-03-20 19:11:06 +0800

    创建项目

---
+++ DownloaderManager/DownloaderManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloaderManager
+//
+//  Created by csy on 15/3/20.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
